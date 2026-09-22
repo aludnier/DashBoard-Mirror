1176bb74685e54854dd834b48f2d42cde08c7728
Ayrton LUDGER-CROSNIER <ayrton.ludger-crosnier@epitech.eu>
2026-09-22 16:56:16 +0200

    refactor: 11 fix db setting to accept external connection

--- DashBoard/server/src/adapters/extern/database/prisma.service.ts
+++ DashBoard/server/src/adapters/extern/database/prisma.service.ts
@@ -14,6 +14,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       connectionLimit: 5,
+      allowPublicKeyRetrieval: true,
     });
 
     super({ adapter });
