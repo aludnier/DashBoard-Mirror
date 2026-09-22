e3b471063404da16cefce41d433f655d5ccb770f
Ayrton LUDGER-CROSNIER <ayrton.ludger-crosnier@epitech.eu>
2026-09-22 16:58:23 +0200

    feat: 11 connect api for connection / keep user connected

--- DashBoard/client/src/titlePage/Home.tsx
+++ DashBoard/client/src/titlePage/Home.tsx
@@ -1,17 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './titleStyle.css'
+import { clearUserSession, getUserSession } from '../client';
+
 
 function Home() {
+  const user = getUserSession()
+
+  const handleLogout = () => {
+    clearUserSession()
+    window.location.reload()
+  }
+
   return (
     <div className="title-page">
       <header className="title-header">
         <Link to="/" className="brand"><h1>Dashboard</h1></Link>
-        <nav className="nav-buttons">
-          <Link to="/Inscription"><button>Inscription</button></Link>
-          <Link to="/Connection"><button>Connection</button></Link>
-        </nav>
+
+        {user ? (
+          <div className="nav-buttons">
+            <span>Bonjour {user.email}</span>
+            <button onClick={handleLogout}>Déconnexion</button>
+          </div>
+        ) : (
+          <nav className="nav-buttons">
+            <Link to="/Inscription"><button>Inscription</button></Link>
+            <Link to="/Connection"><button>Connection</button></Link>
+          </nav>
+        )}
       </header>
     </div>
   )
 }
+
 export default Home
