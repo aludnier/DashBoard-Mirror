932f5fbbc52b966a5b75997c62cee59a1acb3668
Alen Sarsen <alen.sarsen@epitech.eu>
2026-09-25 14:43:15 +0200

    refactor: added necessary changes to the title page
    changed and added the nav bar to the title page

--- DashBoard/client/src/titlePage/Home.tsx
+++ DashBoard/client/src/titlePage/Home.tsx
@@ -1,35 +1,15 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import NavBar from '../components/NavBar'
 import './titleStyle.css'
-import { clearUserSession, getUserSession } from '../client';
-
 
 function Home() {
-  const user = getUserSession()
-
-  const handleLogout = () => {
-    clearUserSession()
-    window.location.reload()
-  }
-
   return (
     <div className="title-page">
-      <header className="title-header">
-        <Link to="/" className="brand"><h1>Dashboard</h1></Link>
-
-        {user ? (
-          <div className="nav-buttons">
-            <span>Bonjour {user.email}</span>
-            <button onClick={handleLogout}>Déconnexion</button>
-          </div>
-        ) : (
-          <nav className="nav-buttons">
-            <Link to="/Inscription"><button>Inscription</button></Link>
-            <Link to="/Connection"><button>Connection</button></Link>
-          </nav>
-        )}
-      </header>
+      <NavBar>
+        <Link to="/Inscription"><button>Inscription</button></Link>
+        <Link to="/Connection"><button>Connection</button></Link>
+      </NavBar>
     </div>
   )
 }
-
 export default Home
